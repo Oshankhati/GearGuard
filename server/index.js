0a5972589e1e46c6160355e38764e90e7e2163fc
satyaranjan2005 <satyaranjannayak2005@gmail.com>
2025-12-27 13:38:15 +0530

    added all routes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the API' });
 });
 
+app.use('/api/auth', require('./routes/authRoute'));
+app.use('/api/departments', require('./routes/department'));
+app.use('/api/maintenance-teams', require('./routes/maintainanceTeam'));
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
